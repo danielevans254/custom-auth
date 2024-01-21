a473251de5683045185e9e18b41c33bea42439e7
danielevans254 <danielevans254@yahoo.com>
2024-01-21 13:06:45 +0800

    Add disabled attribute when submitting
    Set the correct error type for success and error messages, added these to the handleSubmit function

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -5,7 +5,7 @@ import * as z from "zod"
 import { LoginSchema } from "@/schemas"
 import { zodResolver } from '@hookform/resolvers/zod';
 import { login } from "@/actions/login"
-import { useTransition } from "react"
+import { useState, useTransition } from "react"
 import {
   Form,
   FormControl,
@@ -22,10 +22,22 @@ import { FormSuccess } from "../form-success"
 import { Separator } from "@radix-ui/react-separator"
 export const LoginForm = () => {
 
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+
   const [isPending, startTransition] = useTransition();
 
   const handleSubmit = (values: z.infer<typeof LoginSchema>) => {
-    login(values)
+    setError("")
+    setSuccess("")
+    startTransition(() => {
+      login(values)
+        .then((data) => {
+          setError(data.error)
+          setSuccess(data.success)
+        })
+    })
+
   }
 
   const form = useForm<z.infer<typeof LoginSchema>>({
@@ -51,7 +63,6 @@ export const LoginForm = () => {
           className="space-y-6"
         >
           <div className="space-y-4">
-
             <>
               <FormField
                 control={form.control}
@@ -61,6 +72,7 @@ export const LoginForm = () => {
                     <FormLabel>Email</FormLabel>
                     <FormControl>
                       <Input
+                        disabled={isPending}
                         {...field}
                         placeholder="john.doe@example.com"
                         type="email"
@@ -78,6 +90,7 @@ export const LoginForm = () => {
                     <FormLabel>Password</FormLabel>
                     <FormControl>
                       <Input
+                        disabled={isPending}
                         {...field}
                         placeholder="********"
                         type="password"
@@ -89,9 +102,10 @@ export const LoginForm = () => {
               />
             </>
           </div>
-          <FormError message={""} />
-          <FormSuccess message={""} />
-          <Button type="submit" className="w-full bg-indigo-800/90 shadow-xl hover:bg-indigo-900">Login</Button>
+          <FormError message={error} />
+          <FormSuccess message={success} />
+          <Button disabled={isPending}
+            type="submit" className="w-full bg-indigo-800/90 shadow-xl hover:bg-indigo-900">Login</Button>
           <div className="flex flex-col items-center justify-center pt-0">
             <Separator orientation="horizontal" className="absolute w-80 border-t-2" />
             <span className="text-gray-600 px-6 z-10 bg-white">OR</span>
