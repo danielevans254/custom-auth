ec7d8654c59a48a2fd9336f910ab96002e60778b
danielevans254 <danielevans254@yahoo.com>
2024-02-17 14:10:05 +0800

    style: update style

--- components/form-error.tsx
+++ components/form-error.tsx
@@ -10,7 +10,7 @@ export const FormError = ({
   if (!message) return null;
   return (
     <div>
-      <div className="flex items-center space-x-2 bg-red-100 p-3">
+      <div className="flex items-center space-x-2 bg-red-200/70 p-3">
         <ExclamationTriangleIcon className="w-4 h-4 text-red-600" />
         <span className="text-red-600 text-sm">{message}</span>
       </div>
