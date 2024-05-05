e93a70ecb35daecf661c8483b7bb7a27088cc9bf
danielevans254 <danielevans254@yahoo.com>
2024-05-05 14:19:20 +0800

    refactor: Refactor and update login and password reset schemas

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,6 +1,5 @@
 import * as z from 'zod';
 
-
 export const LoginSchema = z.object({
   email: z.string().email({
     message: 'Please enter a valid email'
@@ -21,3 +20,25 @@ export const RegisterSchema = z.object({
     message: 'Please enter your name'
   })
 });
+
+export const ResetPasswordSchema = z.object({
+  email: z.string().email({
+    message: 'Please enter a valid email'
+  })
+});
+
+// TODO: We can add a confirmation password field here, add a check to ensure they match
+// TODO: It doesn't show the error message when the passwords don't match, but it doesn't allow the user to submit the form which is correct
+export const SetNewPasswordSchema = z
+  .object({
+    password: z.string().min(8, {
+      message: 'Password must be at least 8 characters long'
+    }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirm"],
+  });
+
+// SetNewPasswordSchema.parse({ password: "asdf", confirm: "qwer" });
