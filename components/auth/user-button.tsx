149b9b5ecc607663bdfc7f8535e51d865ecec9c3
danielevans254 <danielevans254@yahoo.com>
2024-05-06 15:47:50 +0800

    feat: add more component to use new UI components and improve user experience

--- components/auth/user-button.tsx
+++ components/auth/user-button.tsx
@@ -2,44 +2,74 @@
 import { logout } from "@/actions/logout"
 import { useCurrentUser } from "@/hooks/use-current-user"
 import { useState } from "react"
-import { Button } from "../ui/button"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import Link from "next/link";
+import { FaUser, FaUserAstronaut, FaUserCircle } from "react-icons/fa";
+import { MdDoorBack } from "react-icons/md";
+import { ExitIcon, GearIcon } from "@radix-ui/react-icons";
+
 
 // TODO: Add a user button component that displays the user's name and other needed things and a sign out button
 // TODO: When leaving the user button, the dropdown should close
 const UserButton = () => {
-  const user = useCurrentUser()
-  const [open, setOpen] = useState(false)
+  const user = useCurrentUser();
 
   const signOutOnClick = () => {
     logout()
   }
 
-  const toggleOpen = () => {
-    setOpen(!open)
+  function truncate(str: string | undefined, num: number) {
+    return str && str.length > num ? str.slice(0, num) + "..." : str;
   }
 
-  if (!user) return null
+  if (!user) return null;
 
   return (
-    <div className="relative">
-      <Button onClick={toggleOpen} variant="secondary" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-        {user.name} 🔑
-      </Button>
-      {open && (
-        <div className="absolute right-0 mt-2 w-50 bg-white rounded-md overflow-hidden shadow-xl z-10">
-          <div className="">
-            <div className="flex justify-between">
-              <div className="block px-4 py-2 text-sm">{user.name}</div>
-              <div className="block px-4 py-2 text-sm ">{user.role}</div>
-            </div>
-            <div className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">{user.email}</div>
-            <div className="border-t border-gray-100"></div>
-            <Button onClick={signOutOnClick} type="submit" variant="ghost" size="lg" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-200 w-full">
-              Sign out
-            </Button>
-          </div>
-        </div>
-      )}
+    <div className="flex items-center">
+      <DropdownMenu>
+        <DropdownMenuTrigger className="rounded-full mr-2 ">
+          <Avatar>
+            <AvatarImage src={user?.image || ""} />
+            <AvatarFallback className="bg-sky-700">
+              <FaUser className="size-1/2 text-white" />
+            </AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="">
+          <DropdownMenuLabel>{truncate(user.email ?? '', 16)}</DropdownMenuLabel>
+          <DropdownMenuLabel>{user.role}</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <FaUserAstronaut className="size-4 mr-2" />
+            <Link href="/profile">Profile</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <GearIcon className="size-4 mr-2" />
+            <Link href="/settings">Settings</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={signOutOnClick} className="cursor-pointer">
+            <ExitIcon className="size-4 mr-2" />
+            Sign Out
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
 
   )
